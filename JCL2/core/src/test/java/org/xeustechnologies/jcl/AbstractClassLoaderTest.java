3a31107e81b41fb36c97e6bc4436ef55a4589f33
Jerome <jguibert@gmail.com>
2012-06-22 17:27:35 +0200

    extends test case

--- JCL2/core/src/test/java/org/xeustechnologies/jcl/AbstractClassLoaderTest.java
+++ JCL2/core/src/test/java/org/xeustechnologies/jcl/AbstractClassLoaderTest.java
@@ -18,17 +18,30 @@ public class AbstractClassLoaderTest {
 		AbstractClassLoader classLoader = new AbstractClassLoader() {
 		};
 
-		assertNotNull(classLoader.getSystemLoader());
-		assertNotNull(classLoader.getThreadLoader());
-		assertNotNull(classLoader.getParentLoader());
-		assertNotNull(classLoader.getCurrentLoader());
-		assertNotNull(classLoader.getOsgiBootLoader());
+		assertNotNull("SystemLoader should not be null", classLoader.getSystemLoader());
+		assertNotNull("ThreadLoader should not be null", classLoader.getThreadLoader());
+		assertNotNull("ParentLoader should not be null", classLoader.getParentLoader());
+		assertNotNull("CurrentLoader should not be null", classLoader.getCurrentLoader());
+		assertNotNull("OsgiBootLoader should not be null", classLoader.getOsgiBootLoader());
 
-		assertEquals(50, classLoader.getSystemLoader().getOrder());
-		assertEquals(40, classLoader.getThreadLoader().getOrder());
-		assertEquals(30, classLoader.getParentLoader().getOrder());
-		assertEquals(10, classLoader.getCurrentLoader().getOrder());
-		assertEquals(0, classLoader.getOsgiBootLoader().getOrder());
+		assertEquals("SystemLoader order should be 50", 50, classLoader.getSystemLoader().getOrder());
+		assertEquals("ThreadLoader order should be 40", 40, classLoader.getThreadLoader().getOrder());
+		assertEquals("ParentLoader order should be 30", 30, classLoader.getParentLoader().getOrder());
+		assertEquals("CurrentLoader order should be 20", 20, classLoader.getCurrentLoader().getOrder());
+		assertEquals("OsgiBootLoader order should be 0", 0, classLoader.getOsgiBootLoader().getOrder());
+
+	}
+
+	@Test
+	public void checkDefaultEnabledProxy() {
+		AbstractClassLoader classLoader = new AbstractClassLoader() {
+		};
+
+		assertEquals(Configuration.isCurrentLoaderEnabled(), classLoader.getCurrentLoader().isEnabled());
+		assertEquals(Configuration.isParentLoaderEnabled(), classLoader.getParentLoader().isEnabled());
+		assertEquals(Configuration.isThreadContextLoaderEnabled(), classLoader.getThreadLoader().isEnabled());
+		assertEquals(Configuration.isSystemLoaderEnabled(), classLoader.getSystemLoader().isEnabled());
+		assertEquals(Configuration.isOsgiBootDelegationEnabled(), classLoader.getOsgiBootLoader().isEnabled());
 
 	}
 }
