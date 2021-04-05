39a9c7ca1090f7c81587f9a574311fcf980d6840
jichan <joseph@jc-lab.net>
2021-04-05 14:22:20 +0900

    Support OpenJDK for tests

--- JCL2/spring/src/test/java/org/xeustechnologies/jcl/spring/SpringTest.java
+++ JCL2/spring/src/test/java/org/xeustechnologies/jcl/spring/SpringTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.xeustechnologies.jcl.context.JclContext;
 
 import java.lang.reflect.InvocationTargetException;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 @RunWith(JUnit4.class)
 public class SpringTest extends TestCase {
@@ -22,6 +27,12 @@ public class SpringTest extends TestCase {
         Object test2 = appContext.getBean( "test2" );
 
         assertEquals( "org.xeustechnologies.jcl.JarClassLoader", test1.getClass().getClassLoader().getClass().getName() );
-        assertEquals( "sun.misc.Launcher$AppClassLoader", test2.getClass().getClassLoader().getClass().getName() );
+        assertThat(
+                test2.getClass().getClassLoader().getClass().getName(),
+                anyOf(
+                        equalTo("sun.misc.Launcher$AppClassLoader"),
+                        equalTo("jdk.internal.loader.ClassLoaders$AppClassLoader")
+                )
+        );
     }
 }
